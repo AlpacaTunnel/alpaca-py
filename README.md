c33bf5de5a2c3ed106127133ae8a13548b449e65
AlpacaTunnel <alpaca.tunnel@gmail.com>
2020-04-30 23:53:54 +0800

    remove encryption to improve performance

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # alpaca-py
 
 A Python re-implementation of alpaca-tunnel. Last version compatible with alpaca-tunnel is v1.0, header changed after that.
+
+Branch plain-text: remove encryption to improve performance, also removed some items in header.
--- alpaca/pkt_out.py
+++ alpaca/pkt_out.py
@@ -1,18 +1,15 @@
 """
 Read packet from tunif, encrypt and send to socket.
 """
-from typing import Tuple, List
+from typing import List
 import logging
-import time
 import os
 import random
-from Crypto.Cipher import AES
 
 from .vpn import VPN
 from .peer import PeerAddr
 from .header import Header
-from .ip_packet import IPPacket
-# from .ip_packet_bytearray import IPPacket
+from .ip_packet import IPHeader
 
 logger = logging.getLogger(__name__)
 
@@ -37,23 +34,8 @@ class PktOut:
         self._fill_outter_pkt()
         self._fill_dst_addrs()
 
-    def _encrypt_body(self, header: Header, psk: bytes) -> bytes:
-        aes_block_length = ((header.length + 15) // 16) * 16
-        padding = os.urandom(aes_block_length - header.length)
-
-        cipher = AES.new(psk, AES.MODE_CBC, header.get_iv())
-        return cipher.encrypt(self.body + padding)
-
-    def _get_icv(self, psk: bytes):
-        cipher = AES.new(psk, AES.MODE_ECB)
-        return cipher.encrypt(self.header.to_network())
-
     def _fill_header(self):
-        body = self.body
-        ip = IPPacket().from_network(body)
-        logger.debug(ip)
-
-        ip_h = ip.header
+        ip_h = IPHeader().from_network(self.body[0: IPHeader.LENGTH])
 
         if ip_h.version != 4:
             logger.debug('not support version: %s', ip_h.version)
@@ -61,68 +43,28 @@ class PktOut:
             return
 
         h = self.header
-        h.length = len(body)
-        h.ttl = 15
+        h.length = len(self.body)
+        h.magic = self.vpn.MAGIC
 
         h.src_id = self.vpn.id
-
-        if (self.vpn.network & self.vpn.NETMASK) == (ip_h.src_ip & self.vpn.NETMASK):
-            logger.debug('source IP is within tunnel network')
-            h.src_inside = 1
-            if self.vpn.do_nat:
-                ip.snat(self.vpn.virtual_net + h.src_id)
-        else:
-            h.src_inside = 0
-            logger.debug('source IP is outside tunnel network')
-
-        if (self.vpn.network & self.vpn.NETMASK) == (ip_h.dst_ip & self.vpn.NETMASK):
-            logger.debug('dest IP is within tunnel network')
-            h.dst_inside = 1
+        if self.vpn.network == (ip_h.dst_ip & self.vpn.NETMASK):
             h.dst_id = ip_h.dst_ip & self.vpn.IDMASK
-            if self.vpn.do_nat:
-                ip.dnat(self.vpn.virtual_net + h.dst_id)
         else:
-            logger.debug('dest IP is outside tunnel network')
-            h.dst_inside = 0
-            # TODO: currently, only use gateway as dst_id. Should get dst_id from local route.
             h.dst_id = self.vpn.gateway
-
-        if h.dst_id == 0:
-            logger.debug('no dst id to send')
-            self.valid = False
-            return
-
-        # body changed after NAT
-        if self.vpn.do_nat:
-            self.body = ip.to_network()
-
-        logger.debug(ip)
-
-        h.magic = self.vpn.MAGIC
 
         self.vpn.update_timestamp_seq()
         h.timestamp = self.vpn.TIMESTAMP
         h.sequence = self.vpn.SEQUENCE
-        h.random = os.urandom(2)
 
         logger.debug(h)
 
     def _fill_outter_pkt(self):
-        h = self.header
-        header_cipher = self.vpn.group_cipher.encrypt(h.to_network())
-
-        bigger_id = max(h.src_id, h.dst_id)
-        psk = self.vpn.peers.pool[bigger_id].psk
-
-        icv = self._get_icv(psk)
-        body_cipher = self._encrypt_body(h, psk)
-
-        if h.length > 500:
+        if self.header.length > 500:
             padding = b''
         else:
             padding = os.urandom(random.randint(250, 800))
 
-        self.outter_pkt = b''.join([header_cipher, icv, body_cipher, padding])
+        self.outter_pkt = b''.join([self.header.to_network(), self.body, padding])
 
     def _fill_dst_addrs(self):
         self.dst_addrs = self.vpn.get_dst_addrs(self.header.src_id, self.header.dst_id)
